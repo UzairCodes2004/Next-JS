f6f9c11d483695b24cd927f59961a913c3ee19df
UzairCodes2004 <raouzair70@gmail.com>
2026-06-19 14:16:33 +0500

    Zod validation

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,5 +1,6 @@
 import { log } from "console";
 import { NextRequest,NextResponse } from "next/server";
+import schema from "../schema";
 
 interface Props{
     params:Promise<{id:number}>
@@ -9,24 +10,28 @@ const {id}=await params
 
 if(id>10)
     return NextResponse.json({error:'User not found'},{status:404})
-return NextResponse.json({id :1 ,name:'uzair'})
-if(id==2)
-    return NextResponse.json({id:2,name:'Uzi'})
+return NextResponse.json({id :1 ,name:'uzair'},{
+    
+})
+
 }
 
 //PUT object Validate the request body if valid return 400 fetch the user with the given id 
 export async function PUT(request:NextRequest,{params}:Props) {
     try {
         const {id}=await params
-        const body = await request.json()
-        if(!body.name)
-           { return NextResponse.json({error:'Name is required'})}
+        const body = await 
+        request.json()
+const validation =schema.safeParse(body);
+
+        if(!validation.success)
+           { return NextResponse.json(validation.error.issues)}
         if (id>10)
         { return NextResponse.json({error:'User not found'})}
         
         console.log('Entering Put req')
         return NextResponse.json({id:id,name:body.name})
-        console.log('Exiting Put req')
+        
     } catch(error) {
         return NextResponse.json({error:'Invalid json '},{
             status:400
