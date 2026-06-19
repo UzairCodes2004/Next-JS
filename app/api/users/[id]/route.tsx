a3b54a26b6fe766484a768485105da01f5434ad3
UzairCodes2004 <raouzair70@gmail.com>
2026-06-19 13:20:51 +0500

    PUT API request

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,3 +1,4 @@
+import { log } from "console";
 import { NextRequest,NextResponse } from "next/server";
 
 interface Props{
@@ -9,4 +10,26 @@ const {id}=await params
 if(id>10)
     return NextResponse.json({error:'User not found'},{status:404})
 return NextResponse.json({id :1 ,name:'uzair'})
+if(id==2)
+    return NextResponse.json({id:2,name:'Uzi'})
+}
+
+//PUT object Validate the request body if valid return 400 fetch the user with the given id 
+export async function PUT(request:NextRequest,{params}:Props) {
+    try {
+        const {id}=await params
+        const body = await request.json()
+        if(!body.name)
+           { return NextResponse.json({error:'Name is required'})}
+        if (id>10)
+        { return NextResponse.json({error:'User not found'})}
+        
+        console.log('Entering Put req')
+        return NextResponse.json({id:id,name:body.name})
+        console.log('Exiting Put req')
+    } catch(error) {
+        return NextResponse.json({error:'Invalid json '},{
+            status:400
+        })
+    }
 }
